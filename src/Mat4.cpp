bc6ed9a35b3cb1659bbbbb83f165545173753493
Tyler Hanf <tyler.hanf@corp.advancedspace.com@ADVSP-H9MJ7Y2.corp.advancedspace.com>
2022-07-14 20:10:47 -0600

    Improved speeds by caching look ups

--- src/Mat4.cpp
+++ src/Mat4.cpp
@@ -136,19 +136,38 @@ Mat4 Mat4::LookAt(Vec3 eye, Vec3 at, Vec3 up) {
 // Uses Laplace Expansion Theorem: 
 // https://www.geometrictools.com/Documentation/LaplaceExpansionTheorem.pdf
 float Mat4::Determinant() {
-	float c5 = (M[2][2] * M[3][3]) - (M[2][3] * M[3][2]);
-	float c4 = (M[2][1] * M[3][3]) - (M[2][3] * M[3][1]);
-	float c3 = (M[2][1] * M[3][2]) - (M[2][2] * M[3][1]);
-	float c2 = (M[2][0] * M[3][3]) - (M[2][3] * M[3][0]);
-	float c1 = (M[2][0] * M[3][2]) - (M[2][2] * M[3][0]);
-	float c0 = (M[2][0] * M[3][1]) - (M[2][1] * M[3][0]);
 
-	float s0 = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
-	float s1 = (M[0][0] * M[1][2]) - (M[0][2] * M[1][0]);
-	float s2 = (M[0][0] * M[1][3]) - (M[0][3] * M[1][0]);
-	float s3 = (M[0][1] * M[1][2]) - (M[0][2] * M[1][1]);
-	float s4 = (M[0][1] * M[1][3]) - (M[0][3] * M[1][1]);
-	float s5 = (M[0][2] * M[1][3]) - (M[0][3] * M[1][2]);
+	// Speed up time by reducing look ups repeatedly
+	float m00 = M[0][0];
+	float m01 = M[0][1];
+	float m02 = M[0][2];
+	float m03 = M[0][3];
+	float m10 = M[1][0];
+	float m11 = M[1][1];
+	float m12 = M[1][2];
+	float m13 = M[1][3];
+	float m20 = M[2][0];
+	float m21 = M[2][1];
+	float m22 = M[2][2];
+	float m23 = M[2][3];
+	float m30 = M[3][0];
+	float m31 = M[3][1];
+	float m32 = M[3][2];
+	float m33 = M[3][3];
+
+	float c5 = (m22 * m33) - (m23 * m32);
+	float c4 = (m21 * m33) - (m23 * m31);
+	float c3 = (m21 * m32) - (m22 * m31);
+	float c2 = (m20 * m33) - (m23 * m30);
+	float c1 = (m20 * m32) - (m22 * m30);
+	float c0 = (m20 * m31) - (m21 * m30);
+
+	float s0 = (m00 * m11) - (m01 * m10);
+	float s1 = (m00 * m12) - (m02 * m10);
+	float s2 = (m00 * m13) - (m03 * m10);
+	float s3 = (m01 * m12) - (m02 * m11);
+	float s4 = (m01 * m13) - (m03 * m11);
+	float s5 = (m02 * m13) - (m03 * m12);
 
 	float determinant = s0 * c5 - s1 * c4 + s2 * c3 + s3 * c2 - s4 * c1 + s5 * c0;
 
@@ -158,20 +177,36 @@ float Mat4::Determinant() {
 // Uses Laplace Expansion Theorem: 
 // https://www.geometrictools.com/Documentation/LaplaceExpansionTheorem.pdf
 void Mat4::Invert() {
+	float m00 = M[0][0];
+	float m01 = M[0][1];
+	float m02 = M[0][2];
+	float m03 = M[0][3];
+	float m10 = M[1][0];
+	float m11 = M[1][1];
+	float m12 = M[1][2];
+	float m13 = M[1][3];
+	float m20 = M[2][0];
+	float m21 = M[2][1];
+	float m22 = M[2][2];
+	float m23 = M[2][3];
+	float m30 = M[3][0];
+	float m31 = M[3][1];
+	float m32 = M[3][2];
+	float m33 = M[3][3];
 
-	float c5 = (M[2][2] * M[3][3]) - (M[2][3] * M[3][2]);
-	float c4 = (M[2][1] * M[3][3]) - (M[2][3] * M[3][1]);
-	float c3 = (M[2][1] * M[3][2]) - (M[2][2] * M[3][1]);
-	float c2 = (M[2][0] * M[3][3]) - (M[2][3] * M[3][0]);
-	float c1 = (M[2][0] * M[3][2]) - (M[2][2] * M[3][0]);
-	float c0 = (M[2][0] * M[3][1]) - (M[2][1] * M[3][0]);
+	float c5 = (m22 * m33) - (m23 * m32);
+	float c4 = (m21 * m33) - (m23 * m31);
+	float c3 = (m21 * m32) - (m22 * m31);
+	float c2 = (m20 * m33) - (m23 * m30);
+	float c1 = (m20 * m32) - (m22 * m30);
+	float c0 = (m20 * m31) - (m21 * m30);
 
-	float s0 = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
-	float s1 = (M[0][0] * M[1][2]) - (M[0][2] * M[1][0]);
-	float s2 = (M[0][0] * M[1][3]) - (M[0][3] * M[1][0]);
-	float s3 = (M[0][1] * M[1][2]) - (M[0][2] * M[1][1]);
-	float s4 = (M[0][1] * M[1][3]) - (M[0][3] * M[1][1]);
-	float s5 = (M[0][2] * M[1][3]) - (M[0][3] * M[1][2]);
+	float s0 = (m00 * m11) - (m01 * m10);
+	float s1 = (m00 * m12) - (m02 * m10);
+	float s2 = (m00 * m13) - (m03 * m10);
+	float s3 = (m01 * m12) - (m02 * m11);
+	float s4 = (m01 * m13) - (m03 * m11);
+	float s5 = (m02 * m13) - (m03 * m12);
 
 	float determinant = s0 * c5 - s1 * c4 + s2 * c3 + s3 * c2 - s4 * c1 + s5 * c0;
 
@@ -181,25 +216,25 @@ void Mat4::Invert() {
 	}
 	else {
 		Mat4 adj;
-		adj[0][0] = (M[1][1] * c5) - (M[1][2] * c4) + (M[1][3] * c3);
-		adj[0][1] = (M[0][1] * c5) + (M[0][2] * c4) - (M[0][3] * c3);
-		adj[0][2] = (M[3][1] * s5) - (M[3][2] * s4) + (M[3][3] * s3);
-		adj[0][3] = -(M[2][1] * s5) + (M[2][2] * s4) - (M[2][3] * s3);
+		adj[0][0] = (m11 * c5) - (m12 * c4) + (m13 * c3);
+		adj[0][1] = (m01 * c5) + (m02 * c4) - (m03 * c3);
+		adj[0][2] = (m31 * s5) - (m32 * s4) + (m33 * s3);
+		adj[0][3] = -(m21 * s5) + (m22 * s4) - (m23 * s3);
 
-		adj[1][0] = -(M[1][0] * c5) + (M[1][2] * c2) - (M[1][3] * c1);
-		adj[1][1] = (M[0][0] * c5) - (M[0][2] * c2) + (M[0][3] * c1);
-		adj[1][2] = -(M[3][0] * s5) + (M[3][2] * s2) - (M[3][3] * s1);
-		adj[1][3] = (M[2][0] * s5) - (M[2][2] * s2) + (M[2][3] * s1);
+		adj[1][0] = -(m10 * c5) + (m12 * c2) - (m13 * c1);
+		adj[1][1] = (m00 * c5) - (m02 * c2) + (m03 * c1);
+		adj[1][2] = -(m30 * s5) + (m32 * s2) - (m33 * s1);
+		adj[1][3] = (m20 * s5) - (m22 * s2) + (m23 * s1);
 
-		adj[2][0] = (M[1][0] * c4) - (M[1][1] * c2) + (M[1][3] * c0);
-		adj[2][1] = -(M[0][0] * c4) + (M[0][1] * c2) - (M[0][3] * c0);
-		adj[2][2] = (M[3][0] * s4) - (M[3][1] * s2) + (M[3][3] * s0);
-		adj[2][3] = -(M[2][0] * s4) + (M[2][1] * s2) - (M[2][3] * s0);
+		adj[2][0] = (m10 * c4) - (m11 * c2) + (m13 * c0);
+		adj[2][1] = -(m00 * c4) + (m01 * c2) - (m03 * c0);
+		adj[2][2] = (m30 * s4) - (m31 * s2) + (m33 * s0);
+		adj[2][3] = -(m20 * s4) + (m21 * s2) - (m23 * s0);
 
-		adj[3][0] = -(M[1][0] * c3) + (M[1][1] * c1) - (M[1][2] * c0);
-		adj[3][1] = (M[0][0] * c3) - (M[0][1] * c1) + (M[0][2] * c0);
-		adj[3][2] = -(M[3][0] * s3) + (M[3][1] * s1) - (M[3][2] * s0);
-		adj[3][3] = (M[2][0] * s3) - (M[2][1] * s1) + (M[2][2] * s0);
+		adj[3][0] = -(m10 * c3) + (m11 * c1) - (m12 * c0);
+		adj[3][1] = (m00 * c3) - (m01 * c1) + (m02 * c0);
+		adj[3][2] = -(m30 * s3) + (m31 * s1) - (m32 * s0);
+		adj[3][3] = (m20 * s3) - (m21 * s1) + (m22 * s0);
 
 		*this = adj / determinant;
 	}
@@ -256,52 +291,119 @@ void Mat4::SetErrorMat() {
 }
 
 Mat4 Mat4::operator*(const Mat4& other) const {
+	float m00 = M[0][0];
+	float m01 = M[0][1];
+	float m02 = M[0][2];
+	float m03 = M[0][3];
+	float m10 = M[1][0];
+	float m11 = M[1][1];
+	float m12 = M[1][2];
+	float m13 = M[1][3];
+	float m20 = M[2][0];
+	float m21 = M[2][1];
+	float m22 = M[2][2];
+	float m23 = M[2][3];
+	float m30 = M[3][0];
+	float m31 = M[3][1];
+	float m32 = M[3][2];
+	float m33 = M[3][3];
+
+	float o00 = other[0][0];
+	float o01 = other[0][1];
+	float o02 = other[0][2];
+	float o03 = other[0][3];
+	float o10 = other[1][0];
+	float o11 = other[1][1];
+	float o12 = other[1][2];
+	float o13 = other[1][3];
+	float o20 = other[2][0];
+	float o21 = other[2][1];
+	float o22 = other[2][2];
+	float o23 = other[2][3];
+	float o30 = other[3][0];
+	float o31 = other[3][1];
+	float o32 = other[3][2];
+	float o33 = other[3][3];
+
 	Mat4 temp;
-	temp[0][0] = M[0][0] * other[0][0] + M[0][1] * other[1][0] + M[0][2] * other[2][0] + M[0][3] * other[3][0];
-	temp[0][1] = M[0][0] * other[0][1] + M[0][1] * other[1][1] + M[0][2] * other[2][1] + M[0][3] * other[3][1];
-	temp[0][2] = M[0][0] * other[0][2] + M[0][1] * other[1][2] + M[0][2] * other[2][2] + M[0][3] * other[3][2];
-	temp[0][3] = M[0][0] * other[0][3] + M[0][1] * other[1][3] + M[0][2] * other[2][3] + M[0][3] * other[3][3];
+	temp[0][0] = m00 * o00 + m01 * o10 + m02 * o20 + m03 * o30;
+	temp[0][1] = m00 * o01 + m01 * o11 + m02 * o21 + m03 * o31;
+	temp[0][2] = m00 * o02 + m01 * o12 + m02 * o22 + m03 * o32;
+	temp[0][3] = m00 * o03 + m01 * o13 + m02 * o23 + m03 * o33;
 
-	temp[1][0] = M[1][0] * other[0][0] + M[1][1] * other[1][0] + M[1][2] * other[2][0] + M[1][3] * other[3][0];
-	temp[1][1] = M[1][0] * other[0][1] + M[1][1] * other[1][1] + M[1][2] * other[2][1] + M[1][3] * other[3][1];
-	temp[1][2] = M[1][0] * other[0][2] + M[1][1] * other[1][2] + M[1][2] * other[2][2] + M[1][3] * other[3][2];
-	temp[1][3] = M[1][0] * other[0][3] + M[1][1] * other[1][3] + M[1][2] * other[2][3] + M[1][3] * other[3][3];
+	temp[1][0] = m10 * o00 + m11 * o10 + m12 * o20 + m13 * o30;
+	temp[1][1] = m10 * o01 + m11 * o11 + m12 * o21 + m13 * o31;
+	temp[1][2] = m10 * o02 + m11 * o12 + m12 * o22 + m13 * o32;
+	temp[1][3] = m10 * o03 + m11 * o13 + m12 * o23 + m13 * o33;
 
-	temp[2][0] = M[2][0] * other[0][0] + M[2][1] * other[1][0] + M[2][2] * other[2][0] + M[2][3] * other[3][0];
-	temp[2][1] = M[2][0] * other[0][1] + M[2][1] * other[1][1] + M[2][2] * other[2][1] + M[2][3] * other[3][1];
-	temp[2][2] = M[2][0] * other[0][2] + M[2][1] * other[1][2] + M[2][2] * other[2][2] + M[2][3] * other[3][2];
-	temp[2][3] = M[2][0] * other[0][3] + M[2][1] * other[1][3] + M[2][2] * other[2][3] + M[2][3] * other[3][3];
+	temp[2][0] = m20 * o00 + m21 * o10 + m22 * o20 + m23 * o30;
+	temp[2][1] = m20 * o01 + m21 * o11 + m22 * o21 + m23 * o31;
+	temp[2][2] = m20 * o02 + m21 * o12 + m22 * o22 + m23 * o32;
+	temp[2][3] = m20 * o03 + m21 * o13 + m22 * o23 + m23 * o33;
 
-	temp[3][0] = M[3][0] * other[0][0] + M[3][1] * other[1][0] + M[3][2] * other[2][0] + M[3][3] * other[3][0];
-	temp[3][1] = M[3][0] * other[0][1] + M[3][1] * other[1][1] + M[3][2] * other[2][1] + M[3][3] * other[3][1];
-	temp[3][2] = M[3][0] * other[0][2] + M[3][1] * other[1][2] + M[3][2] * other[2][2] + M[3][3] * other[3][2];
-	temp[3][3] = M[3][0] * other[0][3] + M[3][1] * other[1][3] + M[3][2] * other[2][3] + M[3][3] * other[3][3];
+	temp[3][0] = m30 * o00 + m31 * o10 + m32 * o20 + m33 * o30;
+	temp[3][1] = m30 * o01 + m31 * o11 + m32 * o21 + m33 * o31;
+	temp[3][2] = m30 * o02 + m31 * o12 + m32 * o22 + m33 * o32;
+	temp[3][3] = m30 * o03 + m31 * o13 + m32 * o23 + m33 * o33;
 	return temp;
 }
 
 // TODO: Fix
 Mat4& Mat4::operator*=(const Mat4& other) {
+	float m00 = M[0][0];
+	float m01 = M[0][1];
+	float m02 = M[0][2];
+	float m03 = M[0][3];
+	float m10 = M[1][0];
+	float m11 = M[1][1];
+	float m12 = M[1][2];
+	float m13 = M[1][3];
+	float m20 = M[2][0];
+	float m21 = M[2][1];
+	float m22 = M[2][2];
+	float m23 = M[2][3];
+	float m30 = M[3][0];
+	float m31 = M[3][1];
+	float m32 = M[3][2];
+	float m33 = M[3][3];
+
+	float o00 = other[0][0];
+	float o01 = other[0][1];
+	float o02 = other[0][2];
+	float o03 = other[0][3];
+	float o10 = other[1][0];
+	float o11 = other[1][1];
+	float o12 = other[1][2];
+	float o13 = other[1][3];
+	float o20 = other[2][0];
+	float o21 = other[2][1];
+	float o22 = other[2][2];
+	float o23 = other[2][3];
+	float o30 = other[3][0];
+	float o31 = other[3][1];
+	float o32 = other[3][2];
+	float o33 = other[3][3];
+
 	Mat4 temp;
-	temp[0][0] = M[0][0] * other[0][0] + M[0][1] * other[1][0] + M[0][2] * other[2][0] + M[0][3] * other[3][0];
-	temp[0][1] = M[0][0] * other[0][1] + M[0][1] * other[1][1] + M[0][2] * other[2][1] + M[0][3] * other[3][1];
-	temp[0][2] = M[0][0] * other[0][2] + M[0][1] * other[1][2] + M[0][2] * other[2][2] + M[0][3] * other[3][2];
-	temp[0][3] = M[0][0] * other[0][3] + M[0][1] * other[1][3] + M[0][2] * other[2][3] + M[0][3] * other[3][3];
+	temp[0][0] = m00 * o00 + m01 * o10 + m02 * o20 + m03 * o30;
+	temp[0][1] = m00 * o01 + m01 * o11 + m02 * o21 + m03 * o31;
+	temp[0][2] = m00 * o02 + m01 * o12 + m02 * o22 + m03 * o32;
+	temp[0][3] = m00 * o03 + m01 * o13 + m02 * o23 + m03 * o33;
 
-	temp[1][0] = M[1][0] * other[0][0] + M[1][1] * other[1][0] + M[1][2] * other[2][0] + M[1][3] * other[3][0];
-	temp[1][1] = M[1][0] * other[0][1] + M[1][1] * other[1][1] + M[1][2] * other[2][1] + M[1][3] * other[3][1];
-	temp[1][2] = M[1][0] * other[0][2] + M[1][1] * other[1][2] + M[1][2] * other[2][2] + M[1][3] * other[3][2];
-	temp[1][3] = M[1][0] * other[0][3] + M[1][1] * other[1][3] + M[1][2] * other[2][3] + M[1][3] * other[3][3];
+	temp[1][0] = m10 * o00 + m11 * o10 + m12 * o20 + m13 * o30;
+	temp[1][1] = m10 * o01 + m11 * o11 + m12 * o21 + m13 * o31;
+	temp[1][2] = m10 * o02 + m11 * o12 + m12 * o22 + m13 * o32;
+	temp[1][3] = m10 * o03 + m11 * o13 + m12 * o23 + m13 * o33;
 
-	temp[2][0] = M[2][0] * other[0][0] + M[2][1] * other[1][0] + M[2][2] * other[2][0] + M[2][3] * other[3][0];
-	temp[2][1] = M[2][0] * other[0][1] + M[2][1] * other[1][1] + M[2][2] * other[2][1] + M[2][3] * other[3][1];
-	temp[2][2] = M[2][0] * other[0][2] + M[2][1] * other[1][2] + M[2][2] * other[2][2] + M[2][3] * other[3][2];
-	temp[2][3] = M[2][0] * other[0][3] + M[2][1] * other[1][3] + M[2][2] * other[2][3] + M[2][3] * other[3][3];
+	temp[2][0] = m20 * o00 + m21 * o10 + m22 * o20 + m23 * o30;
+	temp[2][1] = m20 * o01 + m21 * o11 + m22 * o21 + m23 * o31;
+	temp[2][2] = m20 * o02 + m21 * o12 + m22 * o22 + m23 * o32;
+	temp[2][3] = m20 * o03 + m21 * o13 + m22 * o23 + m23 * o33;
 
-	temp[3][0] = M[3][0] * other[0][0] + M[3][1] * other[1][0] + M[3][2] * other[2][0] + M[3][3] * other[3][0];
-	temp[3][1] = M[3][0] * other[0][1] + M[3][1] * other[1][1] + M[3][2] * other[2][1] + M[3][3] * other[3][1];
-	temp[3][2] = M[3][0] * other[0][2] + M[3][1] * other[1][2] + M[3][2] * other[2][2] + M[3][3] * other[3][2];
-	temp[3][3] = M[3][0] * other[0][3] + M[3][1] * other[1][3] + M[3][2] * other[2][3] + M[3][3] * other[3][3];
-
+	temp[3][0] = m30 * o00 + m31 * o10 + m32 * o20 + m33 * o30;
+	temp[3][1] = m30 * o01 + m31 * o11 + m32 * o21 + m33 * o31;
+	temp[3][2] = m30 * o02 + m31 * o12 + m32 * o22 + m33 * o32;
+	temp[3][3] = m30 * o03 + m31 * o13 + m32 * o23 + m33 * o33;
 	*this = temp;
 	return *this;
 }
